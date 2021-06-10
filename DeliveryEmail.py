329156451c189d21013cb622cde741aecd9655c1
developer <pablo.chavez1992@gmail.com>
2021-06-10 12:54:02 -0400

    Update DeliveryEmail.py

--- DeliveryEmail.py
+++ DeliveryEmail.py
@@ -2,27 +2,28 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def deliveryEmail(Puntuacion_Train, Puntuacion_Test, proximodia, datosUlt):
+def deliveryEmail(puntuaciontrain, puntuaciontest, pronostico, datosUlt):
     msg = MIMEMultipart()
     body = "Estimados " \
            "\n\nEsta es la puntuacion del modelo para predecir el siguiente valor del Bitcoin" \
            "\n\nPuntuacion Train: {} RMSE\nPuntuacion Test: {} RMSE" \
            "\n\nPronostico: {}" \
-           "\n\nValor Real: {}".format(Puntuacion_Train, Puntuacion_Test, proximodia, datosUlt)
+           "\n\nValor Real: {}" \
+           "\n\n\nSaludos".format(puntuaciontrain, puntuaciontest, pronostico, datosUlt)
     msg = MIMEText(body)
 
-    msg['From'] = ''
-    msg['To'] = ''
+    msg['From'] = 'pablo.chavez1992@gmail.com'
+    msg['To'] = 'pablo.chavez1992@gmail.com'
     msg['Subject'] = 'Pronostico Bitcoin'
 
     # Reemplaza estos valores con tus credenciales de Google Mail
-    username = ''
-    password = ''
+    username = 'pablo.chavez1992@gmail.com'
+    password = 'pablo09291017'
 
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     server.login(username, password)
-    server.sendmail('','' ,msg.as_string())
+    server.sendmail('pablo.chavez1992@gmail.com','pablo.chavez1992@gmail.com' ,msg.as_string())
     server.quit()
 
     return print('Mensaje enviado con exito')
