6c2b3bd4bf20d1cdefd9f364ecf106286d9eedfc
developer <pablo.chavez1992@gmail.com>
2021-06-11 14:46:26 -0400

    Update ConnectMysql.py

--- ConnectMysql.py
+++ ConnectMysql.py
@@ -15,24 +15,37 @@ def coneccionDb():
     return dataframe
 
 
-def insertBtc(fecha, precioBtc):
+def insertBtc(precioBtc):
     cnx = mysql.connector.connect(user='root', password='1234',
                                   host='127.0.0.1',
                                   database='bitcoin')
     cursor = cnx.cursor()
     sql = "INSERT INTO bitcoinshist (fecha, euro) VALUES (%s, %s)"
+    fecha = datetime.today().strftime('%Y-%m-%d')
     val = (fecha, precioBtc)
     cursor.execute(sql, val)
     cnx.commit()
 
 
-def insertDatosFinales(puntuaciontrain, puntuaciontest, proximodia):
+def insertDatosFinales(puntuaciontrain, puntuaciontest, pronostico):
     cnx = mysql.connector.connect(user='root', password='1234',
                                   host='127.0.0.1',
                                   database='bitcoin')
     cursor = cnx.cursor()
-    sql = "INSERT INTO datosfinales (puntuaciontrain, puntuaciontest, proximodia, fecha) VALUES (%s, %s, %s, %s)"
+    sql = "INSERT INTO datosfinales (puntuaciontrain, puntuaciontest, pronostico, fecha) VALUES (%s, %s, %s, %s)"
     fecha = datetime.today().strftime('%Y-%m-%d')
-    val = (puntuaciontrain, puntuaciontest, proximodia, fecha)
+    val = (puntuaciontrain, puntuaciontest, pronostico, fecha)
+    cursor.execute(sql, val)
+    cnx.commit()
+
+
+def insertCryptoMarker(compra, venta, ultPrecio):
+    cnx = mysql.connector.connect(user='root', password='1234',
+                                  host='127.0.0.1',
+                                  database='bitcoin')
+    cursor = cnx.cursor()
+    sql = "INSERT INTO cryptomarker (compra, venta, ultPrecio, fecha) VALUES (%s, %s, %s, %s)"
+    fecha = datetime.today().strftime('%Y-%m-%d')
+    val = (compra, venta, ultPrecio, fecha)
     cursor.execute(sql, val)
     cnx.commit()
